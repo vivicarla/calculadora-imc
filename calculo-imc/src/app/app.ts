a82223dd79422f94b95b0850b901f0a0d6bfb513
Carla Viviane <carla_dourado@estudante.sesisenai.org.br>
2026-03-17 14:29:43 -0300

    .

--- calculo-imc/src/app/app.ts
+++ calculo-imc/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {Imc} from './imc/imc';
+import {Teste} from './teste/teste';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Imc],
+  imports: [RouterOutlet, Imc, Teste],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
